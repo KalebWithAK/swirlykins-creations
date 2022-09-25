967eb0f61da1fdc7eaf2f346387afe375926f72e
KalebWithAK <kad9603@gmail.com>
2022-09-24 21:07:32 -0400

    added category card

---
+++ client/src/components/categoryCard.js
@@ -0,0 +1,9 @@
+import '../styles/components/category-card.css'
+export default function CategoryCard({ /* img, name */ }) {
+  return (
+    <div className='category-card'>
+      <img alt='category img' />
+      <h3>Category Name</h3>
+    </div>
+  )
+}
